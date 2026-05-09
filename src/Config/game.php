8176fd4af8df8c199c245485002cd9460abb57d0
lzhsus <1434253600@qq.com>
2026-05-09 16:21:17 +0800

    add game config

---
+++ src/Config/game.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'default_user_id' => 1,
+    'pet_action_reward_coin' => 5,
+    'pet_action_reward_exp' => 5,
+    'pet_level_exp_base' => 20,
+    'pet_level_up_coin' => 30,
+    'max_status_value' => 100,
+    'pet_actions' => [
+        'feed' => ['field' => 'hunger', 'value' => 15],
+        'bath' => ['field' => 'clean', 'value' => 15],
+        'play' => ['field' => 'mood', 'value' => 15],
+    ],
+];

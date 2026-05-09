ea475e42ef57807cd5f584aab226eba48118a5d0
lzhsus <1434253600@qq.com>
2026-05-09 16:46:43 +0800

    update cors headers

--- public/index.php
+++ public/index.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 86400');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 spl_autoload_register(function (string $class): void {
     $prefix = 'App\\';
     $baseDir = dirname(__DIR__) . '/src/';
@@ -19,15 +29,6 @@ spl_autoload_register(function (string $class): void {
 });
 
 use App\Core\Router;
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
 
 $router = new Router();
 $registerRoutes = require dirname(__DIR__) . '/src/Config/routes.php';

7e3ed3f72d94b2409859518abed2dbc5ac92be5b
lzhsus <1434253600@qq.com>
2026-05-09 16:37:26 +0800

    add shop controller

---
+++ src/Controllers/ShopController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Core\Auth;
+use App\Core\Request;
+use App\Core\Response;
+use App\Repositories\JsonGameRepository;
+use App\Services\ShopService;
+
+class ShopController
+{
+    private function service(): ShopService
+    {
+        return new ShopService(new JsonGameRepository());
+    }
+
+    public function list(): void
+    {
+        Response::success([
+            'goods' => $this->service()->list(),
+        ]);
+    }
+
+    public function buy(): void
+    {
+        $userId = Auth::userId();
+
+        if ($userId <= 0) {
+            Response::error('unauthorized', 401, 401);
+        }
+
+        $params = Request::json();
+        $goodsId = (int) ($params['goods_id'] ?? 0);
+
+        Response::success(
+            $this->service()->buy($userId, $goodsId)
+        );
+    }
+}

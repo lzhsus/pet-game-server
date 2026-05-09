432c27dca2a77a196da90eadffe5f59fcc2cd8be
lzhsus <1434253600@qq.com>
2026-05-09 16:43:18 +0800

    add routes config

---
+++ src/Config/routes.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Controllers\AuthController;
+use App\Controllers\UserController;
+use App\Controllers\PetController;
+use App\Controllers\TaskController;
+use App\Controllers\BagController;
+use App\Controllers\ShopController;
+use App\Controllers\RewardController;
+
+return function ($router) {
+    $router->post('/api/auth/login', [AuthController::class, 'login']);
+
+    $router->get('/api/user/profile', [UserController::class, 'profile']);
+
+    $router->get('/api/pet/profile', [PetController::class, 'profile']);
+    $router->post('/api/pet/feed', [PetController::class, 'feed']);
+    $router->post('/api/pet/bath', [PetController::class, 'bath']);
+    $router->post('/api/pet/play', [PetController::class, 'play']);
+
+    $router->get('/api/task/list', [TaskController::class, 'list']);
+    $router->post('/api/task/receive', [TaskController::class, 'receive']);
+
+    $router->get('/api/bag/list', [BagController::class, 'list']);
+
+    $router->get('/api/shop/list', [ShopController::class, 'list']);
+    $router->post('/api/shop/buy', [ShopController::class, 'buy']);
+
+    $router->post('/api/reward/sign', [RewardController::class, 'dailySign']);
+};

ff3fb81c4f8b358868157d309876039996f3439e
lzhsus <1434253600@qq.com>
2026-05-11 12:10:10 +0800

    configure pet action and decay values

--- src/Config/game.php
+++ src/Config/game.php
@@ -2,14 +2,19 @@
 
 return [
     'default_user_id' => 1,
-    'pet_action_reward_coin' => 5,
-    'pet_action_reward_exp' => 5,
+    'pet_action_reward_exp' => 1,
     'pet_level_exp_base' => 20,
     'pet_level_up_coin' => 30,
     'max_status_value' => 100,
+    'status_decay_minutes' => 1,
+    'status_decay_values' => [
+        'hunger' => 5,
+        'clean_value' => 3,
+        'mood' => 4,
+    ],
     'pet_actions' => [
-        'feed' => ['field' => 'hunger', 'value' => 15],
-        'bath' => ['field' => 'clean', 'value' => 15],
-        'play' => ['field' => 'mood', 'value' => 15],
+        'feed' => ['field' => 'hunger', 'value' => 1],
+        'bath' => ['field' => 'clean', 'value' => 1],
+        'play' => ['field' => 'mood', 'value' => 1],
     ],
 ];
